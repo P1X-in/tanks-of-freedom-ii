95841b18a4ff2a1dbcf75776d042fec0b2788eb7
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2021-06-27 00:43:45 +0200

    Added smoke to damaged tiles

--- scenes/tiles/damaged_tile.gd
+++ scenes/tiles/damaged_tile.gd
@@ -1,6 +1,13 @@
 extends "res://scenes/tiles/tile.gd"
 
 onready var explosion = $"explosion"
+onready var smoke = $"smoke"
+
+export var is_smoking = false
+
+func _ready():
+    if self.is_smoking:
+        self.smoke.set_emitting(true)
 
 func show_explosion():
     self.explosion.explode_a_bit()
