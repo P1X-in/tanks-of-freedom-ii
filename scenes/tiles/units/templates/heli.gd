b462f4c9c23c257deaa39340a029d895b9bcd770
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2022-08-26 14:46:43 +0200

    Fixed not hiding shadows for heli rotors

--- scenes/tiles/units/templates/heli.gd
+++ scenes/tiles/units/templates/heli.gd
@@ -1,3 +1,10 @@
 extends "res://scenes/tiles/units/unit.gd"
 
 var passenger = null
+
+func disable_shadow():
+    .disable_shadow()
+
+    for child in $"mesh_anchor/mesh/rotor".get_children():
+        if child is MeshInstance:
+            child.cast_shadow = 0
