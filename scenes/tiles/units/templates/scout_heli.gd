48b8b89253e9f61cf0a88be35eeb01b158de90d7
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2023-08-31 19:36:41 +0200

    Fixed heli rotors, added missing materials in menu

--- scenes/tiles/units/templates/scout_heli.gd
+++ scenes/tiles/units/templates/scout_heli.gd
@@ -1,12 +1,12 @@
 extends "res://scenes/tiles/units/unit.gd"
 
 func disable_shadow():
-    super.disable_shadow()
+	super.disable_shadow()
 
-    for child in $"mesh_anchor/mesh/rotor".get_children():
-        if child is MeshInstance3D:
-            child.cast_shadow = 0
-        if child is Node3D:
-            for subchild in child.get_children():
-                if subchild is MeshInstance3D:
-                    subchild.cast_shadow = 0
+	for child in $"mesh_anchor/mesh/rotor".get_children():
+		if child is MeshInstance3D:
+			child.cast_shadow = 0
+		if child is Node3D:
+			for subchild in child.get_children():
+				if subchild is MeshInstance3D:
+					subchild.cast_shadow = 0
