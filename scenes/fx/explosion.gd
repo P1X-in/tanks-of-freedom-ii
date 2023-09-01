d3e4a757e5161668dfc92d42d5219193cb8e7758
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2023-09-01 14:48:12 +0200

    Fixed some particle effects and projectile tweening

--- scenes/fx/explosion.gd
+++ scenes/fx/explosion.gd
@@ -7,26 +7,26 @@ extends Node3D
 @onready var heal = $"heal"
 
 func grab_sfx_effect(unit):
-    var audio_player = unit.give_sfx_effect("die")
-    if audio_player == null:
-        return
+	var audio_player = unit.give_sfx_effect("die")
+	if audio_player == null:
+		return
 
-    audio_player.connect("finished", Callable(audio_player, "queue_free"))
-    $"audio".add_child(audio_player)
-    audio_player.play()
+	audio_player.connect("finished", Callable(audio_player, "queue_free"))
+	$"audio".add_child(audio_player)
+	audio_player.play()
 
 func explode():
-    self.smoke.set_emitting(true)
-    self.main.set_emitting(true)
+	self.smoke.set_emitting(true)
+	self.main.set_emitting(true)
 
 func explode_a_bit():
-    self.small_main.set_emitting(true)
+	self.small_main.set_emitting(true)
 
 func puff_some_smoke():
-    self.smoke.set_emitting(true)
+	self.smoke.set_emitting(true)
 
 func rain_bless():
-    self.bless.set_emitting(true)
+	self.bless.set_emitting(true)
 
 func rain_heal():
-    self.heal.set_emitting(true)
+	self.heal.set_emitting(true)
