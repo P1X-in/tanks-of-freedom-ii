baedd72495b2c399eedc8ba2905a8eb5301f43a6
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2023-10-04 20:29:14 +0200

    Admiral brain that bombs the enemy

--- scenes/board/logic/ai/brains/brains.gd
+++ scenes/board/logic/ai/brains/brains.gd
@@ -1,48 +1,54 @@
 
 var brains = {
-    "hq" : preload("res://scenes/board/logic/ai/brains/hq_brain.gd").new(),
-    "barracks" : preload("res://scenes/board/logic/ai/brains/barracks_brain.gd").new(),
-    "factory" : preload("res://scenes/board/logic/ai/brains/factory_brain.gd").new(),
-    "airfield" : preload("res://scenes/board/logic/ai/brains/airfield_brain.gd").new(),
+	"hq" : preload("res://scenes/board/logic/ai/brains/hq_brain.gd").new(),
+	"barracks" : preload("res://scenes/board/logic/ai/brains/barracks_brain.gd").new(),
+	"factory" : preload("res://scenes/board/logic/ai/brains/factory_brain.gd").new(),
+	"airfield" : preload("res://scenes/board/logic/ai/brains/airfield_brain.gd").new(),
 
-    "infantry" : preload("res://scenes/board/logic/ai/brains/infantry_brain.gd").new(),
-    "tank" : preload("res://scenes/board/logic/ai/brains/tank_brain.gd").new(),
-    "heli" : preload("res://scenes/board/logic/ai/brains/heli_brain.gd").new(),
-    "mobile_infantry" : preload("res://scenes/board/logic/ai/brains/mobile_infantry_brain.gd").new(),
-    "rocket_artillery" : preload("res://scenes/board/logic/ai/brains/rocket_artillery_brain.gd").new(),
-    "scout" : preload("res://scenes/board/logic/ai/brains/scout_brain.gd").new(),
+	"infantry" : preload("res://scenes/board/logic/ai/brains/infantry_brain.gd").new(),
+	"tank" : preload("res://scenes/board/logic/ai/brains/tank_brain.gd").new(),
+	"heli" : preload("res://scenes/board/logic/ai/brains/heli_brain.gd").new(),
+	"mobile_infantry" : preload("res://scenes/board/logic/ai/brains/mobile_infantry_brain.gd").new(),
+	"rocket_artillery" : preload("res://scenes/board/logic/ai/brains/rocket_artillery_brain.gd").new(),
+	"scout" : preload("res://scenes/board/logic/ai/brains/scout_brain.gd").new(),
 
-    "npc" : preload("res://scenes/board/logic/ai/brains/npc_brain.gd").new(),
-    "hero" : preload("res://scenes/board/logic/ai/brains/hero_brain.gd").new()
+	"hero_admiral" : preload("res://scenes/board/logic/ai/brains/heroes/admiral_brain.gd").new(),
+
+	"npc" : preload("res://scenes/board/logic/ai/brains/npc_brain.gd").new(),
+	"hero" : preload("res://scenes/board/logic/ai/brains/hero_brain.gd").new()
 }
 
 var assigned_brains = {
-    "modern_airfield" : self.brains['airfield'],
-    "modern_barracks" : self.brains['barracks'],
-    "modern_factory" : self.brains['factory'],
-    "modern_hq" : self.brains['hq'],
-    "steampunk_airfield" : self.brains['airfield'],
-    "steampunk_barracks" : self.brains['barracks'],
-    "steampunk_factory" : self.brains['factory'],
-    "steampunk_hq" : self.brains['hq'],
-    "futuristic_airfield" : self.brains['airfield'],
-    "futuristic_barracks" : self.brains['barracks'],
-    "futuristic_factory" : self.brains['factory'],
-    "futuristic_hq" : self.brains['hq'],
-    "feudal_airfield" : self.brains['airfield'],
-    "feudal_barracks" : self.brains['barracks'],
-    "feudal_factory" : self.brains['factory'],
-    "feudal_hq" : self.brains['hq'],
+	"modern_airfield" : self.brains['airfield'],
+	"modern_barracks" : self.brains['barracks'],
+	"modern_factory" : self.brains['factory'],
+	"modern_hq" : self.brains['hq'],
+	"steampunk_airfield" : self.brains['airfield'],
+	"steampunk_barracks" : self.brains['barracks'],
+	"steampunk_factory" : self.brains['factory'],
+	"steampunk_hq" : self.brains['hq'],
+	"futuristic_airfield" : self.brains['airfield'],
+	"futuristic_barracks" : self.brains['barracks'],
+	"futuristic_factory" : self.brains['factory'],
+	"futuristic_hq" : self.brains['hq'],
+	"feudal_airfield" : self.brains['airfield'],
+	"feudal_barracks" : self.brains['barracks'],
+	"feudal_factory" : self.brains['factory'],
+	"feudal_hq" : self.brains['hq'],
 }
 
 func get_brain_for_template(template_name):
-    if self.assigned_brains.has(template_name):
-        return self.assigned_brains[template_name]
+	if self.assigned_brains.has(template_name):
+		return self.assigned_brains[template_name]
 
-    return null
+	return null
 
 func get_brain_for_unit(unit):
-    if self.brains.has(unit.unit_class):
-        return self.brains[unit.unit_class]
+	if unit.unit_class == "hero":
+		if self.brains.has(unit.template_name):
+			return self.brains[unit.template_name]
 
-    return null
+	if self.brains.has(unit.unit_class):
+		return self.brains[unit.unit_class]
+
+	return null
---
+++ scenes/board/logic/ai/brains/heroes/admiral_brain.gd
@@ -0,0 +1,80 @@
+extends "res://scenes/board/logic/ai/brains/hero_brain.gd"
+
+func _gather_ability_actions(entity_tile, ap, board):
+	var unit = entity_tile.unit.tile
+	var ability = unit.active_abilities[0]
+
+	if not unit.has_moves():
+		return []
+	if ability.ap_cost > ap or ability.is_on_cooldown():
+		return []
+
+	var approach_target_tile
+	var path
+	var actions = []
+	var action
+	var tiles_in_range = []
+	var targets_in_range = []
+	var unit_range = unit.get_move()
+
+	var approachable_targets = []
+	var target_search_range = 8
+
+	if unit_range > ap:
+		unit_range = ap
+
+	for enemy_unit_tile in self.pathfinder.enemy_units:
+		approach_target_tile = self.pathfinder.enemy_units[enemy_unit_tile]
+
+		path = self.pathfinder.get_path_to_tile(approach_target_tile)
+
+		if path.size() > 3 and path.size() <= target_search_range:
+			approachable_targets.append(approach_target_tile)
+
+	for enemy_unit_tile in approachable_targets:
+		path = self.pathfinder.get_path_to_tile(enemy_unit_tile)
+		if path.size() - 1 > ability.ability_range:
+			var steps_needed = path.size() - 1 - (ability.ability_range - 1)
+			if steps_needed > unit_range - 1:
+				steps_needed = unit_range - 1
+			action = self._approach_action(entity_tile, path, steps_needed)
+			if action != null:
+				action.value = _calculate_blast_value(unit, enemy_unit_tile)
+				if action.value >= 40:
+					actions.append(action)
+
+	
+	targets_in_range = []
+	tiles_in_range = board.ability_markers.get_all_tiles_in_ability_range(ability, entity_tile)
+
+	for tile in tiles_in_range:
+		if ability.is_tile_applicable(tile, entity_tile):
+			targets_in_range.append(tile)
+
+	for target_tile in targets_in_range:
+		action = self._ability_action(ability, target_tile)
+		ability.active_source_tile = entity_tile
+		action.delay = 0.5
+		action.value = _calculate_blast_value(unit, target_tile)
+		if action.value >= 40:
+			actions.append(action)
+
+	return actions
+
+func _calculate_blast_value(source, target_tile):
+	var tiles_in_blast_area = [target_tile]
+	var final_value = 0
+
+	tiles_in_blast_area += target_tile.neighbours.values()
+
+	for tile in tiles_in_blast_area:
+		if tile.has_enemy_unit(source.side, source.team):
+			final_value += tile.unit.tile.unit_value
+			if tile.unit.tile.hp <= 5:
+				final_value += 10
+		if tile.has_friendly_unit(source.side) or tile.has_allied_unit(source.team):
+			final_value -= tile.unit.tile.unit_value
+			if tile.unit.tile.hp <= 5:
+				final_value -= 10
+
+	return final_value
