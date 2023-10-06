487c7b7869e47348981f44f8667d649c7db5251d
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2023-10-06 10:38:50 +0200

    Prince brain that supports unit in battle, Warlord brain that promotes most valuable target

--- scenes/board/logic/ai/brains/brains.gd
+++ scenes/board/logic/ai/brains/brains.gd
@@ -18,6 +18,8 @@ var brains = {
 	"hero_general" : preload("res://scenes/board/logic/ai/brains/heroes/general_brain.gd").new(),
 	"hero_gentleman" : preload("res://scenes/board/logic/ai/brains/heroes/gentleman_brain.gd").new(),
 	"hero_noble" : preload("res://scenes/board/logic/ai/brains/heroes/noble_brain.gd").new(),
+    "hero_prince" : preload("res://scenes/board/logic/ai/brains/heroes/prince_brain.gd").new(),
+    "hero_warlord" : preload("res://scenes/board/logic/ai/brains/heroes/warlord_brain.gd").new(),
 
 	"npc" : preload("res://scenes/board/logic/ai/brains/npc_brain.gd").new(),
 	"hero" : preload("res://scenes/board/logic/ai/brains/hero_brain.gd").new()
---
+++ scenes/board/logic/ai/brains/heroes/warlord_brain.gd
@@ -0,0 +1,68 @@
+extends "res://scenes/board/logic/ai/brains/hero_brain.gd"
+
+func _gather_ability_actions(entity_tile, ap, _board):
+    var unit = entity_tile.unit.tile
+    var ability = unit.active_abilities[0]
+
+    if not unit.has_moves():
+        return []
+    if ability.ap_cost > ap or ability.is_on_cooldown():
+        return []
+
+    var actions = []
+    var target_tile
+    var action
+    var path
+    var interaction_tiles
+    var unit_range = unit.get_move()
+
+    if unit_range > ap:
+        unit_range = ap
+
+    for friendly_unit_tile in self.pathfinder.own_units:
+        target_tile = self.pathfinder.own_units[friendly_unit_tile]
+
+        if not ability.is_tile_applicable(target_tile, entity_tile):
+            continue
+
+        if entity_tile.is_neighbour(target_tile):
+            action = self._ability_action(ability, target_tile)
+            action.delay = 0.5
+            ability.active_source_tile = entity_tile
+            action.value = target_tile.unit.tile.unit_value
+            if target_tile.unit.tile.level == 0:
+                action.value += target_tile.unit.tile.unit_value
+            actions.append(action)
+            continue
+
+        path = self.pathfinder.get_path_to_tile(target_tile)
+
+        if path.size() - 1 > unit_range:
+            action = self._approach_action(entity_tile, path, unit_range - 1)
+            if action != null:
+                action.value = target_tile.unit.tile.unit_value - 20
+                if target_tile.unit.tile.level == 0:
+                    action.value += target_tile.unit.tile.unit_value
+                actions.append(action)
+        else:
+            interaction_tiles = self._get_interaction_tiles(target_tile, entity_tile)
+
+            for interaction_tile in interaction_tiles:
+                path = self.pathfinder.get_path_to_tile(interaction_tile)
+
+                if path.size() - 1 > unit_range - 1:
+                    action = self._approach_action(entity_tile, path, unit_range - 1)
+                    if action != null:
+                        action.value = target_tile.unit.tile.unit_value - 10
+                        if target_tile.unit.tile.level == 0:
+                            action.value += target_tile.unit.tile.unit_value
+                        actions.append(action)
+                else:
+                    action = self._approach_action(entity_tile, path, path.size() - 1)
+                    if action != null:
+                        action.value = target_tile.unit.tile.unit_value - path.size()
+                        if target_tile.unit.tile.level == 0:
+                            action.value += target_tile.unit.tile.unit_value
+                        actions.append(action)
+
+    return actions
